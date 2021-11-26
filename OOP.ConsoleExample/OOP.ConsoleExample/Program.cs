6d3fcf147daccf96bf9a5bad008ab327169760b5
AldoEspinoza1 <74159964+AldoEspinoza1@users.noreply.github.com>
2021-11-26 16:28:13 -0600

    Ejercicio video 1 y 2

---
+++ OOP.ConsoleExample/OOP.ConsoleExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace OOP.ConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+
+            Animal animal = new Animal();
+            animal.AnimalSound();
+
+
+            Dog dog = new Dog();
+            dog.AnimalSound();
+
+            Cat cat = new Cat();
+            cat.AnimalSound();
+
+            Elephant elephant = new Elephant();
+            elephant.AnimalSound();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
