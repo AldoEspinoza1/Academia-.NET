f001e415a3eee0fa26cc4d7684045be3caee4e16
AldoEspinoza1 <74159964+AldoEspinoza1@users.noreply.github.com>
2021-11-26 16:25:30 -0600

    Ejercicio 1 parte 1 y 2

---
+++ DN_1.1_Aldo_Marin_v1.1/AudioManager/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+namespace AudioManager
+{
+    public class Vehicle
+    {
+        public void VehicleSound()
+        {
+            Console.WriteLine("I have several vehicle sounds for you to listen to");
+        }
+    }
+}
---
+++ DN_1.1_Aldo_Marin_v1.1/DN_1.1_Aldo_Marin_v1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+namespace DN_1._1_Jorge_DeLara_v1._1
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            Console.Write("Vehicle sound");
+            Console.WriteLine("Don't have sounds");
+            Vehicle vehicle=new Vehicle();
+            vehicle.VehicleSound();
+            Console.ReadKey();
+
+        }
+    }
+}
